28a5e1226d9fd67e65b6e0252f52c6f1de8af096
Watkins <watkins@sea-watkins-m3.ds.ad.adp.com>
2018-05-17 23:39:24 -0700

    More cleaned up, BnBDetails still fails, will revampt when I learn how to pass propType functions.

--- ecommerce-ui/src/BnbDetails/BnbDetails.test.js
+++ ecommerce-ui/src/BnbDetails/BnbDetails.test.js
@@ -4,14 +4,28 @@ import ReactTestRenderer from 'react-test-renderer';
 import airbnbs from '../airbnbs.json';
 import PropTypes from 'prop-types'
 
-describe('BnbDetails component', () => {
-  it('should render', () => {
-     const match = {
+
+export default class WithContext extends React.Component {
+  static contextTypes = {
+    match: PropTypes.any,
+    addItemToCart: PropTypes.any,
+  }
+
+  render () {
+    const match = {
       params: {
         id: airbnbs[0].id
       }
     }
-    const component = ReactTestRenderer.create(<BnbDetails  match={match} addItemToCart={"test"} />);
+    return (
+      <div><BnbDetails match={match} addItemToCart={"test"} /></div>
+    )
+  }
+}
+
+describe('BnbDetails component', () => {
+  it('should render', () => {
+    const component = ReactTestRenderer.create(<WithContext />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
