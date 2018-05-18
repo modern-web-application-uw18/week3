b4f57829fd639367d9c01a4b5ca073209ef00d3a
Watkins <watkins@sea-watkins-m3.ds.ad.adp.com>
2018-05-17 23:32:27 -0700

    Got BnbDetails test to pass, PropTypes are still failing.

--- ecommerce-ui/src/BnbDetails/BnbDetails.test.js
+++ ecommerce-ui/src/BnbDetails/BnbDetails.test.js
@@ -1,39 +1,17 @@
-// I CANNOT GET THIS TO PASS, I just get endless error messages about how addItemToCart or params is undefined
-
 import React from 'react';
 import BnbDetails from './BnbDetails';
 import ReactTestRenderer from 'react-test-renderer';
 import airbnbs from '../airbnbs.json';
 import PropTypes from 'prop-types'
 
-
-// export default class WithContext extends React.Component {
-//   static contextTypes = {
-//     params: PropTypes.any,
-//     addItemToCart: PropTypes.any,
-//   }
-//   // fake addItemToCart function to try to get test below to pass
-//   // object: airbnbs[0].id,
-//   // params: {
-//   //   id: this.object.id
-//   // }
-//   addItemToCart = (purchaseItem) => {
-//     const test = purchaseItem;
-//   };
-
-//   render () {
-//     return (
-//       <div><BnbDetails obj={airbnbs[0]} params={params} addItemToCart={addItemToCart()} /></div>
-//     )
-//   }
-// }
-
 describe('BnbDetails component', () => {
   it('should render', () => {
-    // const component = ReactTestRenderer.create(
-    //     <WithContext params={{ id: airbnbs[0].id }} />
-    // );
-     const component = ReactTestRenderer.create(<BnbDetails  params={{id: airbnbs[0].id}} addItemToCart={"test"} />);
+     const match = {
+      params: {
+        id: airbnbs[0].id
+      }
+    }
+    const component = ReactTestRenderer.create(<BnbDetails  match={match} addItemToCart={"test"} />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
