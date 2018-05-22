6aab7778a1b4a836078abe3b9b9896e7af00ffd3
Watkins <watkins@sea-watkins-m3.ds.ad.adp.com>
2018-05-21 18:53:27 -0700

    Added MemoryRouter to fix failing test.

--- ecommerce-ui/src/BnbListings/BnbListings.test.js
+++ ecommerce-ui/src/BnbListings/BnbListings.test.js
@@ -2,10 +2,17 @@ import React from 'react';
 import BnbListings from './BnbListings';
 import ReactTestRenderer from 'react-test-renderer';
 import airbnbs from '../airbnbs.json';
+import {
+  MemoryRouter
+} from "react-router-dom";
 
 describe('BnbListings component', () => {
   it('should render', () => {
-    const component = ReactTestRenderer.create(<BnbListings bnb={airbnbs[0]} />);
+    const component = ReactTestRenderer.create(
+      <MemoryRouter>
+        <BnbListings bnb={airbnbs[0]} />
+      </MemoryRouter>
+    );
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
